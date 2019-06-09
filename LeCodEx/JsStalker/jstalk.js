ed25b985bae335e6b88adfccf2bf1acfbba3ff33
kanishk16 <kanishkkalra10@gmail.com>
2019-06-09 13:27:45 +0530

    html + js prompt konnect

---
+++ LeCodEx/JsStalker/jstalk.js
@@ -0,0 +1,7 @@
+var firstname, lastname, age;
+firstname = prompt("What's your firstname?");
+lastname = prompt("What's your lastname?");
+age = prompt("What's your age?");
+
+console.log("You are "+firstname+" "+lastname);
+console.log("You are "+age+" years old");
---
+++ resources/functionality.js
@@ -0,0 +1,22 @@
+const todoList = [
+    {
+        name: 'task1',
+        completed: false
+    },
+    {
+        name: 'task2',
+        completed: false
+    },
+    {
+        name: 'task3',
+        completed: false
+    },
+    {
+        name: 'task4',
+        completed: false
+    },
+    {
+        name: 'task5',
+        completed: false
+    },
+];
